0cefca343ca5e67fad1b78223a2fb18f8b15b7c7
gavinowens22 <107884409+gavinowens22@users.noreply.github.com>
2026-09-17 15:43:39 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,16 @@
 import random
+
+with open("top_english_nouns_lower_100000.txt", "r") as file:
+    words = file.read().splitlines()
+
+passwords = []
+
+num_words = 4
+
+for i in range(num_words):
+    random_word = random.choice(words)
+    random_number = random.randint(0, 9)
+    word_with_number = random_word + str(random_number)
+    passwords.append(word_with_number)
+
+final_password = "-".join(passwords)
